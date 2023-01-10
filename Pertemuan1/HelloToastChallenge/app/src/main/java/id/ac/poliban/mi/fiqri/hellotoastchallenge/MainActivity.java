0958d662eb86382aded90a1c5239346a1b54b1ff
Fiqrisaldi <116248540+Fiqrisaldi@users.noreply.github.com>
2023-01-11 00:16:28 +0700

    Pertemuan1`

---
+++ Pertemuan1/HelloToastChallenge/app/src/main/java/id/ac/poliban/mi/fiqri/hellotoastchallenge/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.poliban.mi.fiqri.hellotoastchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.poliban.mi.fendri.hellotoastchallenge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
